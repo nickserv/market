8c20580d94a3e6205025219eca9c47fb7ee9b8e9
Nick McCurdy <nick@nickmccurdy.com>
2020-04-04 03:38:12 -0400

    Add error message to friend code format

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,5 +2,5 @@ class Player < ApplicationRecord
   validates :island, presence: true
   validates :name, presence: true, uniqueness: { scope: :island }
   validates :nickname, length: { maximum: 10 }
-  validates :friend_code, format: /\ASW(?:-\d{4}){3}\z/, uniqueness: true
+  validates :friend_code, format: { with:  /\ASW(?:-\d{4}){3}\z/, message: 'is not in the format SW-XXXX-XXXX-XXXX' }, uniqueness: true
 end
