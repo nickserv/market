8c7128048083a74dc971154831afb3980deb07d4
Nick McCurdy <nick@nickmccurdy.com>
2020-04-04 03:29:58 -0400

    Add validation

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,6 @@
 class Player < ApplicationRecord
+  validates :island, presence: true
+  validates :name, presence: true, uniqueness: { scope: :island }
+  validates :nickname, length: { maximum: 10 }
+  validates :friend_code, format: /\ASW(?:-\d{4}){3}\z/, uniqueness: true
 end
