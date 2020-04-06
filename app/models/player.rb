199a1d58e318da4a6aee351183e08706ddd6feb3
Nick McCurdy <nick@nickmccurdy.com>
2020-04-06 15:46:28 -0400

    Upcase codes before validation for consistency

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,4 +4,12 @@ class Player < ApplicationRecord
   validates :nickname, length: { maximum: 10 }
   validates :friend_code, format: { with:  /\ASW(?:-\d{4}){3}\z/, message: 'is not in the format SW-XXXX-XXXX-XXXX' }, uniqueness: true
   validates :dodo_code, format: { with:  /\A[\dQWERTYUPASDFGHJKLXCVBNM]{5}\z/, message: 'is not in the format XXXXX' }
+
+  before_validation :upcase_codes
+
+  private
+    def upcase_codes
+      friend_code.upcase!
+      dodo_code.upcase!
+    end
 end
